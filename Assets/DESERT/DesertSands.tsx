f681856654cacc71d17fa0721c6d4ecfe2b9b78d
aclaeczix <alyaalme12@yahoo.com>
2025-03-11 21:11:47 -0600

    Primera Version 1.0

---
+++ Assets/DESERT/DesertSands.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.3" name="DesertSands" tilewidth="16" tileheight="16" tilecount="78" columns="13">
+ <image source="DesertSands.png" width="208" height="96"/>
+</tileset>
