aa5a0b072122012f501669381d71450acfc2577a
AshCampos <ashlycampos00@aragon.unam.mx>
2025-03-14 01:00:55 -0600

    Version 5.1

---
+++ Assets/Scripts/MovPlayer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+public class MovPlayer : MonoBehaviour
+{
+    private Vector2 dirMov;
+    public float velMov;
+    public Rigidbody2D rb;
+    public Animator anim;
+
+    void FixedUpdate()
+    {
+        Movimiento();
+        Animacionesplayer();
+
+    }
+    private void Movimiento()
+    {
+        float movX = Input.GetAxisRaw("Horizontal");
+        float movY = Input.GetAxisRaw("Vertical");
+        Debug.Log($"movX: {movX}, movY: {movY}"); // ← Ver valores en la consola
+        dirMov = new Vector2(movX, movY).normalized;
+        rb.velocity = new Vector2(dirMov.x * velMov, dirMov.y * velMov);
+    }
+
+    private void Animacionesplayer()
+    {
+        anim.SetFloat("movX", dirMov.x);
+        anim.SetFloat("movY", dirMov.y);
+    }
+
+
+}
