a2f841c783c39861d68d6a1f8d7a301a91ef1835
Alexis <alyaalme12@yahoo.com>
2025-03-23 17:55:15 -0600

    Version 8.0

--- Assets/Scripts/MovCamara.cs
+++ Assets/Scripts/MovCamara.cs
@@ -12,6 +12,12 @@ public class MovCamara : MonoBehaviour
     // Variables para configurar los límites de la cámara en cada portal
     private Rect cameraBounds;  // Aquí se guardan los límites de la cámara
 
+    private void Start()
+    {
+        // Establecer los límites del mapa principal cuando comienza el juego
+        cameraBounds = new Rect(-4.5f, -3.1f, 9f, 6.24f);  // (X min, Y min, ancho, altura)
+    }
+
     private void Update()
     {
         if (!isTeleporting && target != null)
@@ -40,56 +46,55 @@ public class MovCamara : MonoBehaviour
         {
             posicionCamara = new Vector3(-5.28f, -13.93f, -6);
             posicionPlayer = new Vector3(-7.276f, -13.23f, 0);
-            // Establecer los límites específicos para este portal
-            cameraBounds = new Rect(-4.5f, -15f, 10f, 5f);  // (X min, Y min, ancho, altura)
+            cameraBounds = new Rect(-4.5f, -15.15f, 9f, 6.3f);
         }
         if (obj.gameObject.CompareTag("portal2"))
         {
             posicionCamara = new Vector3(0.7750003f, 3.510999f, -6);
             posicionPlayer = new Vector3(0.775f, 3.511f, 0);
-            cameraBounds = new Rect(-3f, -1f, 6f, 5f);
+            cameraBounds = new Rect(-4.5f, -3.1f, 9f, 6.2f);
         }
         if (obj.gameObject.CompareTag("portal3"))
         {
             posicionCamara = new Vector3(14.1f, -0.85f, -6);
             posicionPlayer = new Vector3(12.764f, -0.528f, 0);
-            cameraBounds = new Rect(10f, -5f, 8f, 5f);
+            cameraBounds = new Rect(15.5f, -3.1f, 9f, 6.2f);
         }
         if (obj.gameObject.CompareTag("portal4"))
         {
             posicionCamara = new Vector3(-1.322001f, -8.352998f, -6);
             posicionPlayer = new Vector3(-1.422f, -8.553f, 0);
-            cameraBounds = new Rect(-5f, -12f, 6f, 6f);
+            cameraBounds = new Rect(-4.5f, -15.15f, 9f, 6.3f);
         }
         if (obj.gameObject.CompareTag("portal5"))
         {
             posicionCamara = new Vector3(-5.28f, 9.61f, -6);
             posicionPlayer = new Vector3(-6.69f, 9.49f, 0);
-            cameraBounds = new Rect(-4.5f, 5f, 6f, 5f);
+            cameraBounds = new Rect(-4.5f, 8.85f, 9f, 6.3f);
         }
         if (obj.gameObject.CompareTag("portal6"))
         {
             posicionCamara = new Vector3(19.54f, 4.65f, -6);
             posicionPlayer = new Vector3(19.53f, 3.27f, 0);
-            cameraBounds = new Rect(15f, 2f, 8f, 5f);
+            cameraBounds = new Rect(15.5f, -3.1f, 9f, 6.2f);
         }
         if (obj.gameObject.CompareTag("portal7"))
         {
             posicionCamara = new Vector3(-24.92f, 0.3f, -6);
             posicionPlayer = new Vector3(-26.65f, -0.54f, 0);
-            cameraBounds = new Rect(-30f, -3f, 10f, 5f);
+            cameraBounds = new Rect(-24.5f, -3.1f, 9f, 6.25f);
         }
         if (obj.gameObject.CompareTag("portal8"))
         {
             posicionCamara = new Vector3(5.978003f, 11.105f, -6);
             posicionPlayer = new Vector3(6.678f, 11.505f, 0);
-            cameraBounds = new Rect(2f, 9f, 6f, 5f);
+            cameraBounds = new Rect(-4.5f, 8.85f, 9f, 6.3f);
         }
         if (obj.gameObject.CompareTag("portal9"))
         {
             posicionCamara = new Vector3(1.790559f, 3.673067f, -6);
             posicionPlayer = new Vector3(0.775f, 3.511f, 0);
-            cameraBounds = new Rect(-1f, 1f, 3f, 5f);
+            cameraBounds = new Rect(-4.5f, -3.1f, 9f, 6.2f);
         }
 
         // Mover la cámara y al jugador a la nueva posición
