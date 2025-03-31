48d902e2c9cac90027a9b402b88c8e66adf5197f
AshCampos <ashlycampos00@aragon.unam.mx>
2025-03-30 19:29:09 -0600

    Version 9.0

--- Assets/Scripts/Enemigo.cs
+++ Assets/Scripts/Enemigo.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class Enemigo : MonoBehaviour
 {
     public static int vidaEnemigo = 1;
+    private float frecAtaque = 2.5f, tiempoSigAtaque = 0, iniciaConteo;
+
+
 
     void Start()
     {
@@ -14,13 +17,22 @@ public class Enemigo : MonoBehaviour
     
     void Update()
     {
-        
+        if (tiempoSigAtaque > 0)
+        {
+            tiempoSigAtaque = frecAtaque + iniciaConteo - Time.time;
+        }else
+        {
+            tiempoSigAtaque = 0;
+            VidasPlayer.puedePerderVida = 1;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D obj)
     {
         if (obj.tag == "Player")
         {
+            tiempoSigAtaque = frecAtaque;
+            iniciaConteo = Time.time;
             obj.transform.GetComponentInChildren<VidasPlayer>().TomarDaño(1);
         }
     }
