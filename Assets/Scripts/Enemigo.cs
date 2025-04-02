ad586a51722009a221883f55c7e19320c657a240
Alexis <alyaalme12@yahoo.com>
2025-04-02 17:11:24 -0600

    Version 10.0

--- Assets/Scripts/Enemigo.cs
+++ Assets/Scripts/Enemigo.cs
@@ -1,37 +1,109 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Enemigo : MonoBehaviour
 {
     public static int vidaEnemigo = 1;
-    private float frecAtaque = 2.5f, tiempoSigAtaque = 0, iniciaConteo;
+    private float freqAtaque = 2.5f, tiempoSigAtaque = 0, iniciaConteo;
 
+    public Transform personaje;
+    private NavMeshAgent agente;
+    public Transform[] puntosRuta;
+    private int indiceRuta = 0;
+    private bool playerEnRango = false;
+    [SerializeField] private float distanciaDeteccionPlayer;
+    private SpriteRenderer spriteEnemigo;
+    private Transform mirarHacia;
 
+    private void Awake() {
+        agente = GetComponent<NavMeshAgent>();
+        spriteEnemigo = GetComponent<SpriteRenderer>();
+    }
 
     void Start()
     {
         vidaEnemigo = 1;
+        agente.updateRotation = false;
+        agente.updateUpAxis = false;
     }
 
-    
-    void Update()
-    {
+    void Update(){
+
+        this.transform.position = new Vector3(transform.position.x, transform.position.y, 0);
+        float distancia = Vector3.Distance(personaje.position, this.transform.position);
+        if (this.transform.position == puntosRuta[indiceRuta].position)
+        {
+            if (indiceRuta < puntosRuta.Length - 1)
+            {
+                indiceRuta++;
+            }
+            else if (indiceRuta == puntosRuta.Length - 1)
+            {
+                indiceRuta = 0;
+            }
+        }
+
+        if (distancia < distanciaDeteccionPlayer)
+        {
+            playerEnRango = true;
+        }
+        else
+        {
+            playerEnRango = false;
+        }
         if (tiempoSigAtaque > 0)
         {
-            tiempoSigAtaque = frecAtaque + iniciaConteo - Time.time;
-        }else
+            tiempoSigAtaque = freqAtaque + iniciaConteo - Time.time;
+        }
+        else
         {
             tiempoSigAtaque = 0;
             VidasPlayer.puedePerderVida = 1;
+            SigueAlPlayer(playerEnRango);
+            RotaEnemigo();
+        }
+
+    }
+
+    private void SigueAlPlayer(bool playerEnRango)
+    {
+        if (playerEnRango)
+        {
+            agente.SetDestination(personaje.position);
+            mirarHacia = personaje;
+        }
+        else
+        {
+            agente.SetDestination(puntosRuta[indiceRuta].position);
+            mirarHacia = puntosRuta[indiceRuta];
         }
     }
+
+    private void RotaEnemigo()
+    {
+        if (this.transform.position.x > mirarHacia.position.x)
+        {
+            spriteEnemigo.flipX = true;
+            Debug.Log("FipX");
+        }
+        else
+        {
+            spriteEnemigo.flipX = false;
+            Debug.Log("Sin FipX");
+        }
+    }
+
+
+
+
 
     private void OnTriggerEnter2D(Collider2D obj)
     {
         if (obj.tag == "Player")
         {
-            tiempoSigAtaque = frecAtaque;
+            tiempoSigAtaque = freqAtaque;
             iniciaConteo = Time.time;
             obj.transform.GetComponentInChildren<VidasPlayer>().TomarDaño(1);
         }
